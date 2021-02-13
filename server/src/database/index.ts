56d31489506509b2280e10394d053ccde675f9c3
Lenivaya <xocada@gmail.com>
2021-02-13 17:01:40 +0200

    Add persistence using mongodb and graphql

---
+++ server/src/database/index.ts
@@ -0,0 +1,18 @@
+import { MongoClient } from "mongodb";
+import { Database } from "../lib/types";
+
+const { DB_USER, DB_USER_PASSWORD, DB_CLUSTER } = process.env;
+const url = `mongodb+srv://${DB_USER}:${DB_USER_PASSWORD}@${DB_CLUSTER}.mongodb.net/<dbname>?retryWrites=true&w=majority`;
+
+export const connectDatabase = async (): Promise<Database> => {
+  const client = await MongoClient.connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
+
+  const db = client.db("main");
+
+  return {
+    listings: db.collection("test_listings")
+  };
+};
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,24 @@
-import express from "express";
+require("dotenv").config();
+
+import express, { Application } from "express";
 import { ApolloServer } from "apollo-server-express";
 import { typeDefs, resolvers } from "./graphql";
+import { connectDatabase } from "./database";
 
-const app = express();
-const port = 9000;
+const port = process.env.PORT;
 
-const server = new ApolloServer({ typeDefs, resolvers });
-server.applyMiddleware({ app, path: "/api" });
+const mount = async (app: Application) => {
+  const db = await connectDatabase();
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: () => ({ db })
+  });
+  server.applyMiddleware({ app, path: "/api" });
 
-app.listen(port);
+  app.listen(port);
 
-console.log(`[app]: http://localhost:${port}`);
+  console.log(`[app]: http://localhost:${port}`);
+};
+
+mount(express());
