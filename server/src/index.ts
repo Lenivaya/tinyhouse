8718eb66da9325d5e673976ccbeeecc4187fe528
Lenivaya <xocada@gmail.com>
2021-02-22 18:05:25 +0200

    Add server side viewer cookie

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,24 +1,26 @@
 require("dotenv").config();
 
 import express, { Application } from "express";
+import cookieParser from "cookie-parser";
 import { ApolloServer } from "apollo-server-express";
-import { typeDefs, resolvers } from "./graphql";
 import { connectDatabase } from "./database";
-
-const port = process.env.PORT;
+import { typeDefs, resolvers } from "./graphql";
 
 const mount = async (app: Application) => {
   const db = await connectDatabase();
+
+  app.use(cookieParser(process.env.SECRET));
+
   const server = new ApolloServer({
     typeDefs,
     resolvers,
-    context: () => ({ db })
+    context: ({ req, res }) => ({ db, req, res })
   });
+
   server.applyMiddleware({ app, path: "/api" });
+  app.listen(process.env.PORT);
 
-  app.listen(port);
-
-  console.log(`[app]: http://localhost:${port}`);
+  console.log(`[app] : http://localhost:${process.env.PORT}`);
 };
 
 mount(express());
